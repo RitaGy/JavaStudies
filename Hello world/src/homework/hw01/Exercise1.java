3c8349e9cf3b7d0d6ac9c8a5f4acfb8f9eb5f36d
Rita <Rita@Sam.t.hu>
2018-09-30 22:51:49 +0200

    Task 1&2

---
+++ Hello world/src/homework/hw01/Exercise1.java
@@ -0,0 +1,17 @@
+package homework.hw01;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String what = "nothing";
+        String firstName = "John";
+        String delimiter = " ";
+        String lastName = "Snow";
+        String exclamationMark = "!";
+        String doing = "knows";
+        System.out.println(firstName + delimiter + lastName + delimiter + doing + delimiter + what + exclamationMark);
+        
+	}
+
+}
