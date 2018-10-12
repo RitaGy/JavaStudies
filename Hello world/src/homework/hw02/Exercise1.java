fe5614a0e6efb1a06f029792dad8cbf70fc86139
Rita <Rita@192.168.0.206>
2018-10-12 20:38:46 +0200

    Tudom hogy nem jó még.  Task 2 megoldás probléma.

--- Hello world/src/homework/hw02/Exercise1.java
+++ Hello world/src/homework/hw02/Exercise1.java
@@ -3,6 +3,12 @@ package homework.hw02;
 public class Exercise1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        
+     int birthYear = 1987;
+     int actualYear = 2018;
+     int myAge = actualYear - birthYear;
+     System.out.println("My age is " + myAge);
+double minutes = 200;  
+double hours = minutes/60;
+System.out.println (hours);
 	}
 }
---
+++ Hello world/src/homework/hw02/Exercise2.java
@@ -0,0 +1,18 @@
+package homework.hw02;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int randomNumber = 4;
+	boolean odd;
+	if ((randomNumber/2)>=2){
+		odd = false;
+		System.out.println("even " + randomNumber); 
+	}	
+	else {odd = true;
+	System.out.println ("odd " + randomNumber);}
+	}
+
+}
