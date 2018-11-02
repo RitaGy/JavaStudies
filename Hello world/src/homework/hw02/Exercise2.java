13d28ccf28fcb1ba51b8ff1eac2e2301874caf8c
Rita <Rita@Sam.t.hu>
2018-11-02 15:01:06 +0100

    MinMaxLength method

--- Hello world/src/homework/hw02/Exercise2.java
+++ Hello world/src/homework/hw02/Exercise2.java
@@ -5,14 +5,15 @@ public class Exercise2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	int randomNumber = 3;
-	boolean odd;
-	if ((randomNumber/2)*2==randomNumber){
-		odd = false;
-		System.out.println("even " + randomNumber); 
-	}	
-	else {odd = true;
-	System.out.println ("odd " + randomNumber);}
+		int randomNumber = 3;
+		boolean odd;
+		if ((randomNumber / 2) * 2 == randomNumber) {
+			odd = false;
+			System.out.println("even " + randomNumber);
+		} else {
+			odd = true;
+			System.out.println("odd " + randomNumber);
+		}
 	}
 
 }
--- Hello world/src/hw03/NameLength.java
+++ Hello world/src/hw03/NameLength.java
@@ -1,18 +1,38 @@
 package hw03;
 
 public class NameLength {
-	//public int result;
+	// public int result;
 	public int nameLengthAverage(String[] names) {
 		int result2 = 0;
-		for (String name: names) {
-			result2= result2+ name.length();
+		for (String name : names) {
+			result2 = result2 + name.length();
 		}
-		return result2/names.length;
+		return result2 / names.length;
+	}
+
+	public void minMaxLength(String[] input) {
+		String minLength = input[0];
+		String maxLength = input[0];
+		for (String element : input) {
+			if (element.length() > maxLength.length()) {
+				maxLength = element;
+			} else if (element.length() < minLength.length()) {
+				minLength = element;
+			}
+			System.out.println(element);
+		}
+		System.out.println("Shortest name: "+minLength+" (" +minLength.length()+")");
+		System.out.println("Longest name: "+maxLength+" (" +maxLength.length()+")");
+
+
 	}
 
 	public static void main(String[] args) {
 		NameLength nameLength = new NameLength();
-		String[] names={"Jakab", "Eufrozina","John", "Magdi"};
-		System.out.println(nameLength.nameLengthAverage(names));
+		String[] names = { "Jakab", "Eufrozina", "John", "Magdi" };
+		// System.out.println(nameLength.nameLengthAverage(names));
+		nameLength.minMaxLength(names);
+		nameLength.minMaxLength(new String[]{"Alfonz"});
 	}
+
 }
